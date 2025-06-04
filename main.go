d92f5b945d16cf437e63d798987ea49dcb72677b
mmert9008 <michael.mertens.jr@gmail.com>
2025-06-03 21:10:50 -0500

    cleaned up formatting

--- main.go
+++ main.go
@@ -3,62 +3,57 @@ package main
 import "fmt"
 
 func fuelGauge(fuel int) {
-  fmt.Println("You have", fuel, "gallons of fuel remaining")
+	fmt.Println("You have", fuel, "gallons of fuel remaining")
 }
-
 
 func calculateFuel(planet string) int {
-  var fuel int
+	var fuel int
 
-  switch planet {
-  case "Venus":
-    fuel = 300000
-  case "Mercury":
-    fuel = 500000
-  case "Mars":
-    fuel = 700000
-  default:
-    fuel = 0
-  }
-  return fuel
+	switch planet {
+	case "Venus":
+		fuel = 300000
+	case "Mercury":
+		fuel = 500000
+	case "Mars":
+		fuel = 700000
+	default:
+		fuel = 0
+	}
+	return fuel
 }
-
 
 func greetPlanet(planet string) {
-  fmt.Printf("Welcome to %v!\n", planet)
+	fmt.Printf("Welcome to %v!\n", planet)
 }
-
 
 func cantFly() {
-  fmt.Println("We do not have the available fuel to fly there.")
+	fmt.Println("We do not have the available fuel to fly there.")
 }
-
 
 func flyToPlanet(planet string, fuel int) int {
-  var fuelRemaining int
-  var fuelCost int
+	var fuelRemaining int
+	var fuelCost int
 
-  fuelRemaining = fuel
-  fuelCost = calculateFuel(planet)
+	fuelRemaining = fuel
+	fuelCost = calculateFuel(planet)
 
-  if fuelRemaining >= fuelCost {
-    greetPlanet(planet)
-    fuelRemaining -= fuelCost
-  } else {
-    cantFly()
-  }
+	if fuelRemaining >= fuelCost {
+		greetPlanet(planet)
+		fuelRemaining -= fuelCost
+	} else {
+		cantFly()
+	}
 
-  return fuelRemaining
+	return fuelRemaining
 }
 
-
 func main() {
-  var fuel int
-  var planetChoice string
+	var fuel int
+	var planetChoice string
 
-  fuel = 1000000
-  planetChoice = "Venus"
+	fuel = 1000000
+	planetChoice = "Venus"
 
-  fuel = flyToPlanet(planetChoice, fuel)
-  fuelGauge(fuel)
+	fuel = flyToPlanet(planetChoice, fuel)
+	fuelGauge(fuel)
 }
